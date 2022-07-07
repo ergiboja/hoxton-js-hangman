9b69d69b6066c6e620ad3385bde36dd508d9ab4a
Ergi  Boja <perdoruesperdoruesi@gmail.com>
2022-07-07 16:03:40 +0200

    first commit

---
+++ vite-project/src/main.ts
@@ -0,0 +1,290 @@
+import './style.css'
+
+
+let wordList = [
+  {
+      word: "python",
+      characters: ["y","o"],
+      mistakes: 4
+      
+  },
+  {
+      word: "glock",
+      characters: ["g", "k"],
+      mistakes: 4
+  },
+  {
+      word: "aim",
+      characters: ["i"],
+      mistakes: 2
+      
+  },
+  {
+      word: "processor",
+      characters: ["p","c","r"],
+      mistakes: 4
+  },
+  {
+      word: "motherboard",
+      characters: [],
+      mistakes: 4
+      
+  },
+  {
+      word: "ebay",
+      characters: [],
+      mistakes: 4
+    
+  },
+  {
+      word: "gaming",
+      characters: [],
+      mistakes: 4
+      
+  },
+  {
+      word: "coding",
+      characters: [],
+      mistakes: 4
+     
+  },
+  {
+      word: "matrix",
+      characters: [],
+      mistakes: 4
+      
+  },
+  {
+      word: "bugs",
+      characters: [],
+      mistakes: 4
+     
+  },
+  {
+      word: "avatar",
+      characters: [],
+      mistakes: 4
+  
+  },
+  {
+      word: "gif",
+      characters: [],
+      mistakes: 2
+     
+  },
+  {
+      word: "mental",
+      characters: [],
+      mistakes: 4
+    
+  },
+  {
+      word: "map",
+      characters: [],
+      mistakes: 2
+     
+  },
+  {
+      word: "island",
+      characters: [],
+      mistakes: 4
+      
+  },
+  {
+      word: "hockey",
+      characters: [],
+      mistakes: 4
+     
+  },
+  {
+      word: "chess",
+      characters: [],
+      mistakes: 3
+      
+  },
+  {
+      word: "viber",
+      characters: [],
+      mistakes: 3
+     
+  },
+  {
+      word: "github",
+      characters: [],
+      mistakes: 3
+ 
+  },
+  {
+      word: "png",
+      characters: [],
+      mistakes: 2
+     
+  },
+  {
+      word: "silver",
+      characters: [],
+      mistakes: 3
+     
+  },
+  {
+      word: "mobile",
+      characters: [],
+      mistakes: 3
+     
+  },
+  {
+      word: "gpu",
+      characters: [],
+      mistakes: 2
+     
+  },
+  {
+      word: "java",
+      characters: [],
+      mistakes: 2
+  },
+  {
+      word: "google",
+      characters: [],
+      mistakes: 3
+    
+  },
+  {
+      word: "venice",
+      characters: [],
+      mistakes: 3
+      
+  },
+  {
+      word: "excel",
+      characters: [],
+      mistakes: 3
+     
+  },
+  {
+      word: "mysql",
+      characters: [],
+      mistakes: 3
+     
+  },
+  {
+      word: "albania",
+      characters: [],
+      mistakes: 3
+      
+  },
+  {
+      word: "flute",
+      characters: [],
+      mistakes: 3
+     
+  },
+  {
+      word: "crypto",
+      characters: [],
+      mistakes: 3
+      
+  },
+  {
+      word: "tesla",
+      characters: [],
+      mistakes: 3
+     
+  },
+  {
+      word: "mars",
+      characters: [],
+      mistakes: 2
+     
+  },
+  {
+      word: "proxy",
+      characters: [],
+      mistakes: 3
+     
+  },
+  {
+      word: "email",
+      characters: [],
+      mistakes: 3
+     
+  },
+  {
+      word: "html",
+      characters: [],
+      mistakes: 2
+      
+  },
+  {
+      word: "air",
+      characters: [],
+      mistakes: 2
+     
+  },
+  {
+      word: "idea",
+      characters: [],
+      mistakes: 2
+      
+  },
+  {
+      word: "server",
+      characters: [],
+      mistakes: 3
+     
+  },
+  {
+      word: "svg",
+      characters: [],
+      mistakes: 2
+      
+  },
+  {
+      word: "jpeg",
+      characters: [],
+      mistakes: 3
+  },
+  {
+      word: "search",
+      characters: [],
+      mistakes: 3
+    
+  },
+  {
+      word: "key",
+      characters: [],
+      mistakes: 2
+      },
+  {
+      word: "egypt",
+      characters: [],
+      mistakes: 3
+  },
+  {
+      word: "joker",
+      characters: [],
+      mistakes: 3
+  },
+  {
+      word: "dubai",
+      characters: [],
+      mistakes: 3
+     
+  },
+  {
+      word: "photo",
+      characters: [],
+      mistakes: 3
+  },
+  {
+      word: "nile",
+      characters: [],
+      mistakes: 3
+  },
+  {
+      word: "rain",
+      characters: [],
+      mistakes: 3
+
+  },
+]

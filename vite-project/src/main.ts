4dab62a49c8a9ac729a32a614f23171102c61761
Ergi  Boja <perdoruesperdoruesi@gmail.com>
2022-07-08 02:23:07 +0200

    Updates

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -26,265 +26,307 @@ let wordList = [
   },
   {
       word: "motherboard",
-      characters: [],
+      characters: ["m","r","d"],
       mistakes: 4
       
   },
   {
       word: "ebay",
-      characters: [],
-      mistakes: 4
+      characters: ["b","y"],
+      mistakes: 2
     
   },
   {
       word: "gaming",
-      characters: [],
+      characters: ["g","i","g"],
       mistakes: 4
       
   },
   {
       word: "coding",
-      characters: [],
+      characters: ["c","i","g"],
       mistakes: 4
      
   },
   {
       word: "matrix",
-      characters: [],
+      characters: ["m","t","x"],
       mistakes: 4
       
   },
   {
       word: "bugs",
-      characters: [],
-      mistakes: 4
+      characters: ["b","s"],
+      mistakes: 3
      
   },
   {
       word: "avatar",
-      characters: [],
+      characters: ["a","t","r"],
       mistakes: 4
   
   },
   {
       word: "gif",
-      characters: [],
+      characters: ["i"],
       mistakes: 2
      
   },
   {
       word: "mental",
-      characters: [],
+      characters: ["n","l"],
       mistakes: 4
     
   },
   {
       word: "map",
-      characters: [],
+      characters: ["a"],
       mistakes: 2
      
   },
   {
       word: "island",
-      characters: [],
+      characters: ["i","l","d"],
       mistakes: 4
       
   },
   {
       word: "hockey",
-      characters: [],
+      characters: ["h","c","k"],
       mistakes: 4
      
   },
   {
       word: "chess",
-      characters: [],
+      characters: ["s","s"],
       mistakes: 3
       
   },
   {
       word: "viber",
-      characters: [],
+      characters: ["i","b"],
       mistakes: 3
      
   },
   {
       word: "github",
-      characters: [],
+      characters: ["t","h","b"],
       mistakes: 3
  
   },
   {
       word: "png",
-      characters: [],
+      characters: ["n"],
       mistakes: 2
      
   },
   {
       word: "silver",
-      characters: [],
+      characters: ["l","v"],
       mistakes: 3
      
   },
   {
       word: "mobile",
-      characters: [],
+      characters: ["m","b","e"],
       mistakes: 3
      
   },
   {
       word: "gpu",
-      characters: [],
+      characters: ["p"],
       mistakes: 2
      
   },
   {
       word: "java",
-      characters: [],
+      characters: ["j","a"],
       mistakes: 2
   },
   {
       word: "google",
-      characters: [],
+      characters: ["o","o"],
       mistakes: 3
     
   },
   {
       word: "venice",
-      characters: [],
+      characters: ["n","i"],
       mistakes: 3
       
   },
   {
       word: "excel",
-      characters: [],
+      characters: ["e","x"],
       mistakes: 3
      
   },
   {
       word: "mysql",
-      characters: [],
+      characters: ["s","q"],
       mistakes: 3
      
   },
   {
       word: "albania",
-      characters: [],
+      characters: ["a","l"],
       mistakes: 3
       
   },
   {
       word: "flute",
-      characters: [],
+      characters: ["f","t"],
       mistakes: 3
      
   },
   {
       word: "crypto",
-      characters: [],
+      characters: ["y","t"],
       mistakes: 3
       
   },
   {
       word: "tesla",
-      characters: [],
+      characters: ["t","a"],
       mistakes: 3
      
   },
   {
       word: "mars",
-      characters: [],
+      characters: ["a","s"],
       mistakes: 2
      
   },
   {
       word: "proxy",
-      characters: [],
+      characters: ["x","y"],
       mistakes: 3
      
   },
   {
       word: "email",
-      characters: [],
+      characters: ["m","l"],
       mistakes: 3
      
   },
   {
       word: "html",
-      characters: [],
+      characters: ["t","l"],
       mistakes: 2
       
   },
   {
       word: "air",
-      characters: [],
+      characters: ["i"],
       mistakes: 2
      
   },
   {
       word: "idea",
-      characters: [],
+      characters: ["i","a"],
       mistakes: 2
       
   },
   {
       word: "server",
-      characters: [],
+      characters: ["r","v"],
       mistakes: 3
      
   },
   {
       word: "svg",
-      characters: [],
+      characters: ["v"],
       mistakes: 2
       
   },
   {
       word: "jpeg",
-      characters: [],
+      characters: ["p","g"],
       mistakes: 3
   },
   {
       word: "search",
-      characters: [],
+      characters: ["s","r","h"],
       mistakes: 3
     
   },
   {
       word: "key",
-      characters: [],
+      characters: ["y"],
       mistakes: 2
       },
   {
       word: "egypt",
-      characters: [],
+      characters: ["g","y"],
       mistakes: 3
   },
   {
       word: "joker",
-      characters: [],
+      characters: ["j","k"],
       mistakes: 3
   },
   {
       word: "dubai",
-      characters: [],
+      characters: ["d","a"],
       mistakes: 3
      
   },
   {
       word: "photo",
-      characters: [],
+      characters: ["o","t"],
       mistakes: 3
   },
   {
       word: "nile",
-      characters: [],
+      characters: ["l","e"],
       mistakes: 3
   },
   {
       word: "rain",
-      characters: [],
+      characters: ["r","n"],
       mistakes: 3
 
   },
 ]
+
+
+const inputs = document.querySelector('.input');
+let resetBtn = document.querySelector(".buton");
+let mistakes = document.querySelector(".guess span");
+
+
+function randomWord(){
+  let randomObject = wordList[Math.floor(Math.random() * wordList.length)];
+  let word = randomObject.word;
+  mistakes.innerText = randomObject.mistakes
+  console.log(word)
+  console.log(word.length)
+ 
+
+  for(let i = 0; i<word.length; i++){
+    let containerEL = document.querySelector("#input")
+  
+let inputEl = document.createElement('input')
+inputEl.className = "inpt"
+inputEl.type = "text"
+inputEl.maxLength = "1"
+containerEL.append(inputEl)
+ 
+}
+  
+
+}
+// function wordbtn(){ let containerEL = document.querySelector("#input")
+// containerEL.textContent = ''
+// let inputEl = document.createElement('input')
+// inputEl.className = "inpt"
+// inputEl.type = "text"
+// containerEL.append(inputEl)
+// }
+// wordbtn()
+randomWord()
+function reload() {
+  reload = location.reload();
+}
+
+resetBtn.addEventListener("click",reload,false);
